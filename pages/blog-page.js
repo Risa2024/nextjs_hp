e306f9626cf47fcff40067096bbbc0cb470d6e62
Risa Goto <omusubi5656dup@gmail.com>
2024-12-08 19:13:57 +0900

    Post.js 　作成

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -2,9 +2,21 @@ import React from 'react'
 import Layout from '../components/layout';
 import { getAllPostsData } from '../lib/posts';
 
-const Blog = () => {
-  return  <Layout title="Blog">Blog page</Layout>
-    
-}
+const Blog = ({ props }) => {
+  return 
+  <Layout title="Blog">
+     <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+  </Layout>
+ 
+  };
 
 export default Blog;
+
+export async function getStaticProps() { //サーバーサイドでビルド時に実行される
+  const posts = await getAllPostsData
+  return {
+    props: { posts },
+  };
+}
