0ee0d7a763ee89bdf26747ab5b04eefbdefca93c
Risa Goto <omusubi5656dup@gmail.com>
2024-12-08 19:00:09 +0900

    Json API　追加

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout';
+import { getAllPostsData } from '../lib/posts';
 
 const Blog = () => {
   return  <Layout title="Blog">Blog page</Layout>
